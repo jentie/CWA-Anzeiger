90c0ce616a943dd82f5e2461de3e002a2c97032d
jentie <jens@tiemaenner.de>
2021-01-10 16:27:41 +0100

    Update Kommentare CWA Detektor

--- CWA-Detektor/CWA-Detektor.ino
+++ CWA-Detektor/CWA-Detektor.ino
@@ -1,46 +1,55 @@
 /*
    Corona-Warn-App Detektor - zeigt Nutzung von Corona-Warn-Apps in der Umgebung an
 
-   Bluetooth Low Energy (BLE) Scanner mit Filterung nach der Identifikation der
-   Corona-Warn-App (CWA), Anzeige einer oder mehrerer Corona-Warn-Apps in der
-   Umgebung, ohne Speicherung von Adressen oder IDs.
+   Copyright 2021 Jens
 
-   basiert auf dem Beispiel "BLE_scan" aus der Bibliothek "ESP32 BLE Arduino"
+   License: MIT
 
+  
+   Kurzbeschreibung:
+   Bluetooth Low Energy (BLE) Scanner mit Filterung auf Identifikation der
+   Corona-Warn-App (CWA), Anzeige mehrerer Corona-Warn-Apps in der Umgebung,
+   ohne Speicherung von Adressen oder IDs.
+
+   basiert auf dem Beispiel "BLE_scan" der Bibliothek "ESP32 BLE Arduino",
+   siehe auch https://github.com/espressif/arduino-esp32
+   nutzt Arduino-Bibliothek "TM1637" (von Avishay) zur Ansteuerung der Anzeige,
+   siehe auch https://github.com/avishorp/TM1637
+   
    Corona-Warn-App: https://www.bundesregierung.de/breg-de/themen/corona-warn-app
 
-   TODO: board: ESP32 DevKit v1 / Joy-it NodeMCU ESP32 --> "ESP32 Dev Module"
-
-   TODO: Herkunft, Lizenz
+   Hardware: ESP32, bspw. NodeMCU ESP32 --> Board "ESP32 Dev Module"
+             TM1637 4-Digit 7-Segment Display
 
 */
 
 #include <BLEDevice.h>            // Teil des "esp32"-Pakets von Espressif Systems
-                                  // siehe auch https://github.com/espressif/arduino-esp32
-#include <TM1637Display.h>        // Arduino-Bibliothek "TM1637" (von Avishay)
-                                  // siehe auch https://github.com/avishorp/TM1637
+#include <TM1637Display.h>        // Arduino-Bibliothek "TM1637"
 
-#define LEDint 2                  // LED auf dem ESP-Controller: CWA-Indikator
-#define LEDext 13                 // externe LED: zusätzliche CWA melden
+#define LEDint 2                  // LED auf dem ESP-Controller: Bluetooth-Scanindikator
 
-// Anbindung des LED-Display (plus GND & 3.3V)
+// Verbindung Controller <-> LED-Display (plus GND & 3.3V)
 #define CLK 15
 #define DIO 4
 
 int scanTime = 5;                 // in Sekunden
-int idleTime = 2;                 // ... Pause zwischen Scans
+int idleTime = 5;                 // ... Pause zwischen Scans
 
 int numCWA = 0;                   // Zähler für gefundene Corona-Warn-Apps
-int maxCWA = 0;                   // Höchstwert der Anzahl während des Betriebs
-int oldCWA = 0;                   // CWA-Wert aus dem letzten Scanzyklus
-const int thrCWA = 1;             // Schwelle: CWA-Indikator erst darüber aktiv
+const int thrCWA = 5;             // Schwelle: CWA-Anzeige erst darüber aktiv
 
 BLEScan* pBLEScan;
 
 TM1637Display display(CLK, DIO);
 
+
+const byte seg_alle[] = {0xff, 0xff, 0xff, 0xff};   // alle Segmente  --> Muster '8888'
+const byte seg_kein[] = {0x00, 0x00, 0x00, 0x00};   // keine Segmente --> Muster '    '
+const byte seg_leer[] = {0x40, 0x40, 0x40, 0x40};   // nur Segmente G --> Muster '----'
+
+
 /*
-  Auswertung für jedes einzelne, gefundene BLE-Device
+  Auswertung für jedes gefundene BLE-Device
 */
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
@@ -64,29 +73,26 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 
 void setup() {
   Serial.begin(115200);                             // Infos auch über USB-Schnitstelle
-  Serial.println("\nCorona-Warn-App Detektor\n");
+  Serial.println("\n\nCorona-Warn-App Detektor");
 
   pinMode(LEDint, OUTPUT);                          // LED-Pins als Ausgang
-  pinMode(LEDext, OUTPUT);
-  
+
   BLEDevice::init("");                              // BLE-Funktion des ESP32 vorbereiten
   pBLEScan = BLEDevice::getScan();                  // Scan wird vorbereitet
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true);                    // Aktiver Scan, ESP32 sendet Aufforderung
+  pBLEScan->setActiveScan(true);                    // aktiver Scan: ESP32 sendet Aufforderung
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);
 
   display.clear();
   display.setBrightness(3, true);                   // Helligkeit 0...7, Display ein
 
-  for (int i = 0; i < 3; i++) {                     // bei Start blinkt LEDs und Anzeige
+  for (int i = 0; i < 3; i++) {                     // bei Start blinkt LED und Anzeige
     digitalWrite(LEDint, HIGH);
-    digitalWrite(LEDext, HIGH);
-    display.showNumberDec(8888);
+    display.setSegments(seg_alle);                  // zeige '8888'
     delay(100);
     digitalWrite(LEDint, LOW);
-    digitalWrite(LEDext, LOW);
-    display.clear();
+    display.setSegments(seg_kein);                  // zeige '    '
     delay(100);
   }
 }
@@ -96,18 +102,24 @@ void loop() {                                       // ewige Wiederholung des Sc
 
   numCWA = 0;                                       // Zähler für aktuelle CWAs zurücksetzen
   //  Serial.println("scanning...");
+
+  digitalWrite(LEDint, HIGH);                       // Scan-Indikator einschalten
   BLEScanResults foundDevices = pBLEScan->start(scanTime, false);       // BLE Scan starten
+  digitalWrite(LEDint, LOW);                        // Scan-Indikator ausschalten
+
   //  Serial.print("devices found: ");
   //  Serial.println(foundDevices.getCount());
   //  Serial.println("scan done.");
+  
   pBLEScan->clearResults();                         // erfasste BLE-Daten aus dem Scan löschen
 
-  Serial.print("Corona-Warn-Apps: ");               // Ausgaben über USB
-  Serial.print(numCWA);
+  Serial.print("CWAs: ");                           // Ausgaben über USB
+  Serial.println(numCWA);
 
-  display.showNumberDecEx(numCWA, 0b01000000, false, 4, 0);  // Ausgabe auf Display
- 
+  if (numCWA < thrCWA)
+    display.setSegments(seg_leer);                  // keine Ausgabe, zeige '----'
+  else
+    display.showNumberDec(numCWA, false);           // Ausgabe der Anzahl
+
   delay(idleTime * 1000);                           // Pause in Sekunden
-  digitalWrite(LEDint, LOW);                        // CWA-Indikator vor nächstem Scan ausschalten
-  digitalWrite(LEDext, LOW);                        // Indikator "zusätzliche CWA" ausschalten
 }
